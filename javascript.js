a0700051416808595ad2d40af2a22b1aecc6d21f
Taran Saini <34341534+TaranSaini2005@users.noreply.github.com>
2026-02-17 16:35:31 +0000

    Finished scripting

--- javascript.js
+++ javascript.js
@@ -1,43 +1,216 @@
+let valueInput = "";
+let valueOutput = "";
 
-function Add(numberLeft, numberRight)
-{
-    console.log(numberLeft + numberRight);
-}
+let leftInput = "";
+let rightInput = "";
 
-function Multiply(numberLeft, numberRight)
-{
-    console.log(numberLeft * numberRight);
-}
+let opInput = "";
+var calculateInput = false;
+var resetInput = false;
 
-function Subtract(numberLeft, numberRight)
-{
-    console.log(numberLeft - numberRight);
-}
+const screenInput = document.querySelector(".screen-input");
+const screenOutput = document.querySelector(".screen-output");
 
-function Divide(numberLeft, numberRight)
-{
-    console.log(numberLeft / numberRight);
-}
+const btns = document.querySelectorAll(".btn");
+const clr = document.querySelector(".btn-clear");
+const del = document.querySelector(".btn-delete");
 
-function Operate(numberLeft, numberRight, operator)
+btns.forEach(btn => 
 {
-    if (operator == "+")
+    btn.addEventListener("click", () => 
     {
-        Add(numberLeft, numberRight);
+        Input(btn.textContent);
+    });
+});
+
+clr.addEventListener("click", () => Clear());
+del.addEventListener("click", () => Delete());
+
+function Input(input)
+{
+    if (input == "=")
+    {
+        calculateInput = true;
+    }
+    else if (input == ".")
+    {
+        if (resetInput)
+        {  
+            leftInput = valueOutput;
+            rightInput = "";
+            opInput = "";
+            valueOutput = "";
+            resetInput = false;
+        }
+
+        if (rightInput != "")
+        {
+            if (HasDecimal(rightInput) == false)
+            {
+                rightInput += '.';
+            }
+        }
+        else
+        {
+            if (leftInput != "")
+            {
+                if (HasDecimal(leftInput) == false)
+                {
+                    leftInput += '.';
+                }
+            }
+        }
+    }
+    else if (input == "+" || input == "-" || input == "*" || input == "/")
+    {
+        if (resetInput)
+        {  
+            leftInput = valueOutput;
+            rightInput = "";
+            opInput = "";
+            valueOutput = "";
+            resetInput = false;
+        }
+
+        if (leftInput == "" || rightInput != "")
+        {    
+            return;
+        }
+
+        if (opInput == "")
+        {
+            opInput = input;
+        }
+        else
+        {
+            if (opInput != input)
+            {
+                opInput = input;
+            }
+        }
+    }
+    else
+    {
+        if (resetInput)
+        {  
+            leftInput = valueOutput;
+            rightInput = "";
+            opInput = "";
+            valueOutput = "";
+            resetInput = false;
+        }
+
+        if (leftInput == "")
+        {
+            leftInput += input;
+        }
+        else
+        {
+            if (opInput == "")
+            {
+                leftInput += input;
+            }
+            else
+            {
+                rightInput += input;
+            }
+        }
     }
 
-    if (operator == "-")
+    valueInput = leftInput + " " + opInput + " " + rightInput;
+
+    if (calculateInput == true)
     {
-        Subtract(numberLeft, numberRight);
+        if (leftInput != "" && rightInput != "" & opInput != "")
+        {
+            valueInput += " =";
+            valueOutput = Calculate(leftInput, rightInput, opInput);
+            if (HasDecimal(valueOutput))
+            {
+               valueOutput =  Number(valueOutput).toFixed(2);
+            }
+
+            resetInput = true;
+        }
+        
+        calculateInput = false;
     }
 
-    if (operator == "*")
-    {
-        Multiply(numberLeft, numberRight);
-    }
+    Output();
+}
 
-    if (operator == "/")
+function HasDecimal(value)
+{
+    var valueString = value.toString();
+
+    if (valueString.includes('.'))
     {
-        Divide(numberLeft, numberRight);
+        return true;
+    }
+    else
+    {
+        return false;
     }
 }
+
+function Output()
+{
+    screenInput.textContent = valueInput;
+    screenOutput.textContent = valueOutput;
+}
+
+function Clear()
+{
+    valueInput = "";
+    valueOutput = "";
+    leftInput = "";
+    rightInput = "";
+    opInput = "";
+
+    Output();
+}
+
+function Delete()
+{
+    if (rightInput != "")
+    {
+        rightInput = "";
+    }
+    else if (opInput != "")
+    {
+        opInput = "";
+    }
+    else
+    {
+        if (leftInput != "")
+        {
+            leftInput = "";
+        }
+    }
+
+    if (resetInput)
+    {
+        resetInput = false;
+    }
+
+    valueInput = leftInput + " " + opInput + " " + rightInput;
+    valueOutput = "";
+
+    Output();
+}
+
+function Calculate(a, b, op) 
+{
+    a = Number(a);
+    b = Number(b);
+
+    switch (op) 
+    {
+        case "+": return a + b;
+        case "-": return a - b;
+        case "*": return a * b;
+        case "/": return b === 0 ? 0 : a / b;
+    }
+}
+
+Output();
